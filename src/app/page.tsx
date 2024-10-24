6a7fe3b0a1b762dd1f7e44c11a52f7764184a892
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2024-10-24 21:40:02 +1000

    Update page.tsx
    added link to finished app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,15 @@ import { Suspense } from "react";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#443d4a] to-[#1d1d61] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-[#443d4a] to-[#1d1d61] text-white">
+        <a
+          href={
+            "https://crypto-price-app-git-fixes-benjamin-davies-projects-9033428c.vercel.app/"
+          }
+          target="_blank"
+        >
+          Go to Finished App
+        </a>
         <Suspense fallback={<div>Loading...</div>}>
           <DisplayCurrencies />
         </Suspense>
