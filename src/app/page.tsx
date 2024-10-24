c92c9c4c7d12ca9449d31512138d9a9c9f8e55f0
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2024-10-24 21:29:31 +1000

    feat: added link to original app
    added link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { HydrateClient } from "~/trpc/server";
 import CryptoTrackerApp from "./_components/CryptoTrackerApp";
+
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#443d4a] to-[#1d1d61] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gradient-to-b from-[#443d4a] to-[#1d1d61] text-white">
+        <a href={"https://crypto-price-app-two.vercel.app/"} target="_blank">
+          Go to Interview App
+        </a>
         <CryptoTrackerApp />
       </main>
     </HydrateClient>
